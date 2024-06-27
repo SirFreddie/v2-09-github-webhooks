5083e599a554aa9ba493dcb154817594a78ea33f
dana2002 <danacastroman@gmail.com>
2024-06-27 18:35:39 -0300

    last time update

--- src/presentation/services/github.service.ts
+++ src/presentation/services/github.service.ts
@@ -4,8 +4,8 @@ export class GitHubService {
   onStar(payload: GitHubStarPayload): string {
     const { action, repository, starred_at, sender } = payload;
 
+    return `🌟 ${sender.login} starred at ${starred_at} on ${repository.full_name}`;
     if (starred_at) {
-      return `🌟 ${sender.login} starred at ${starred_at} on ${repository.full_name}`;
     }
     return `❌ ${sender.login} removed star at ${repository.full_name}`;
   }
